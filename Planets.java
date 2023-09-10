81ed4869a72906fe82fdd6e1a44384d050554789
toniname <60040991+toniname@users.noreply.github.com>
2023-09-10 23:14:21 +0300

    Add files via upload

---
+++ Planets.java
@@ -0,0 +1,14 @@
+package java_core_grom.mateAkademi.treni.goIt;
+
+class Planets {
+
+    public static final String MERCURY = "Mercury";
+    public static final String VENUS = "Venus";
+    public static final String EARTH = "Earth";
+    public static final String MARS = "Mars";
+    public static final String JUPITER = "Jupiter";
+    public static final String SATURN = "Saturn";
+    public static final String URANUS = "Uranus";
+    public static final String NEPTUNE = "Neptune";
+
+}
