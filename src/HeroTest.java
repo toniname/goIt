1a9279e94103fc1b326a7b650bab8dcaef880183
toniname <drednoyta1@ukr.net>
2023-09-19 22:46:54 +0300

    init commit

--- src/HeroTest.java
+++ src/HeroTest.java
@@ -10,14 +10,23 @@ class HeroTest {
     }
 }
 
-class Hero{
+class Hero {
     private String name;
     private int hp;
 
+    public Hero(String name, int hp) {
+        this.name = name;
+        if (hp >= 0 && hp <=200){
+            this.hp = hp;
+        }else if (hp < 0){
+            this.hp = 0;
+        }else {
+            this.hp = 200;
+        }
+    }
 
     public Hero() {
-        this.name = "Paratrooper";
-        this.hp = 100;
+        this("Paratrooper", 100);
     }
 
     public String getName() {
