48dd411ba514233a0a78fd0f8f65d2707250d35a
toniname <60040991+toniname@users.noreply.github.com>
2023-10-03 23:33:16 +0300

    Add files via upload

---
+++ SpaceportMessages.java
@@ -0,0 +1,9 @@
+
+
+public class SpaceportMessages {
+
+   public static String generateLandingMessage(String shipName, String spaceport, String time){
+
+        return "Spaceship " + shipName + " landing at " + spaceport + ", time is " + time;
+    }
+}
