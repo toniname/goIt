8746bbcbf531a0fec1628aa59d60afeb868438e7
toniname <60040991+toniname@users.noreply.github.com>
2023-10-05 18:05:38 +0300

    Add files via upload

---
+++ Bullet.java
@@ -0,0 +1,12 @@
+class Bullet {
+    {
+        System.out.println("Bullet created!");
+    }
+}
+
+class BulletTest {
+    public static void main(String[] args) {
+        new Bullet();
+        new Bullet();
+    }
+}
