57220f752859c85901789fc2e9502862b4216e63
toniname <60040991+toniname@users.noreply.github.com>
2023-10-31 23:28:16 +0200

    Add files via upload

---
+++ SpaceshipCalc.java
@@ -0,0 +1,15 @@
+
+
+class SpaceshipCalc {
+
+    final int calculateAirBalloons(int passengerCount) {
+
+
+        return passengerCount * 10;
+    }
+
+
+    int calculateBookCount(int passengerCount){
+        return passengerCount * 2;
+    }
+}
