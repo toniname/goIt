b1df35e783af99ec99004e12dd62418456130549
toniname <drednoyta1@ukr.net>
2023-11-25 01:08:21 +0200

    init commit

---
+++ src/modul11/LcGenerator.java
@@ -0,0 +1,23 @@
+package modul11;
+
+import java.util.stream.Stream;
+
+public class LcGenerator {
+
+    public static Stream<Long> generateRandomStream(long seed, long a, long c, long m) {
+        return Stream.iterate(seed, x -> (a * x + c) % m);
+    }
+
+    public static void main(String[] args) {
+        long seed = 123;  // початкове значення seed
+        long a = 25214903917L;
+        long c = 11L;
+        long m = (long) Math.pow(2, 48);
+
+        Stream<Long> randomStream = generateRandomStream(seed, a, c, m);
+
+        randomStream.limit(10).forEach(System.out::println);// Виводимо перші 10 елементів стріму для перевірки
+    }
+}
+
+
