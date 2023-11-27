bd27f5ae843f70d641af9e1d2962b126b9d2f126
toniname <60040991+toniname@users.noreply.github.com>
2023-11-28 00:11:17 +0200

    Add files via upload

---
+++ modul12/Fizzbuzz.java
@@ -0,0 +1,99 @@
+package modul12;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+class FizzBuzz {
+    private final int n;
+    private final BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    public void fizz() throws InterruptedException {
+        for (int i = 3; i <= n; i += 3) {
+            if (i % 5 != 0) {
+                queue.put("fizz");
+            }
+        }
+    }
+
+    public void buzz() throws InterruptedException {
+        for (int i = 5; i <= n; i += 5) {
+            if (i % 3 != 0) {
+                queue.put("buzz");
+            }
+        }
+    }
+
+    public void fizzbuzz() throws InterruptedException {
+        for (int i = 15; i <= n; i += 15) {
+            queue.put("fizzbuzz");
+        }
+    }
+
+    public void number() throws InterruptedException {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 != 0 && i % 5 != 0) {// Умови - яке число відноситься до fizz або buzz.
+                queue.put(Integer.toString(i));
+            } else {
+                String result = queue.take();
+                System.out.println(result);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        FizzBuzz fizzBuzz = new FizzBuzz(n);
+
+        Thread threadA = new Thread(() -> {
+            try {
+                fizzBuzz.fizz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            try {
+                fizzBuzz.buzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadC = new Thread(() -> {
+            try {
+                fizzBuzz.fizzbuzz();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadD = new Thread(() -> {
+            try {
+                fizzBuzz.number();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        // Запускаємо потоки
+        threadA.start();
+        threadB.start();
+        threadC.start();
+        threadD.start();
+
+        // Чекаємо завершення потоків
+        try {
+            threadA.join();
+            threadB.join();
+            threadC.join();
+            threadD.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
