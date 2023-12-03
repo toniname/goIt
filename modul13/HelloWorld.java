f9ef317eeab6bf5d07abef49d13f42973d7d8589
toniname <60040991+toniname@users.noreply.github.com>
2023-12-03 22:48:01 +0200

    Add files via upload

---
+++ modul13/HelloWorld.java
@@ -0,0 +1,70 @@
+package modul13;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class HelloWorld {
+
+    private static final String TEST_URL =
+            "https://jsonplaceholder.typicode.com/users";
+
+    public static void main(String[] args) throws IOException {
+        sendGET();
+        sendPOST();
+    }
+
+    private static void sendGET() throws IOException {
+        URL url = new URL(TEST_URL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        int responseCode = connection.getResponseCode();
+        System.out.println("GET response code: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        } else {
+            System.out.println("GET request not worked");
+        }
+    }
+
+    private static void sendPOST() throws IOException {
+        URL url = new URL(TEST_URL);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        OutputStream os = connection.getOutputStream();
+        os.write(Files.readAllBytes(new File("user.json").toPath()));
+        os.flush();
+        os.close();
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("POST response code: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_CREATED) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            System.out.println(response.toString());
+        } else {
+            System.out.println("POST request not worked");
+        }
+    }
+}
